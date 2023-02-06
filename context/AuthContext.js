96f54a8a978e32c121e60fa60e82e5b59214a71f
Redouanhmeid <hmeid.redouan@gmail.com>
2023-02-06 04:35:44 +0100

    0206

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import React, {createContext, useEffect, useState} from "react";
-import { BASE_URL } from '../client-config'
+import {BASE_URL} from '../client-config'
 
 export const AuthContext = createContext();
 
